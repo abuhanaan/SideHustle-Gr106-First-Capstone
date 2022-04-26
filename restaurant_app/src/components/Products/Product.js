3c39414b24b923ee972114e1e4f60ffab6923e6c
abuhanaan <adewunmi99@gmail.com>
2022-04-26 14:07:06 +0100

    Implemented the menu page

--- restaurant_app/src/components/Products/Product.js
+++ restaurant_app/src/components/Products/Product.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import { MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
 
 function Product() {
   return (
     <div>
-        <h1>This is ths Products Page</h1>
+      <MDBRow>
+
+      <MDBCol md='4'>
+
+      </MDBCol>
+
+      <MDBCol md='8'>
+        <MDBRow>
+          <MDBCol md='4' className='col-example'>
+            <MDBCard>
+              <MDBCardImage
+                src='./images/menu1.png'
+                alt='...'
+                position='top'
+              />
+              <MDBCardBody>
+                <MDBCardTitle>Italian Source Mushroom</MDBCardTitle>
+                <MDBBtn outline color='success'>$19.00</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md='4' className='col-example'>
+            <MDBCard>
+              <MDBCardImage
+                src='./images/menu2.png'
+                alt='...'
+                position='top'
+              />
+              <MDBCardBody>
+                <MDBCardTitle>Salted Fried Chicken</MDBCardTitle>
+                <MDBBtn outline color='success'>$19.00</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol md='4' className='col-example'>
+            <MDBCard>
+              <MDBCardImage
+                src='./images/menu3.png'
+                alt='...'
+                position='top'
+              />
+              <MDBCardBody>
+                <MDBCardTitle>Fried Potato w/Garlic</MDBCardTitle>
+                <MDBBtn outline color='success'>$19.00</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md='4' className='col-example'>
+            <MDBCard>
+              <MDBCardImage
+                src='./images/menu4.png'
+                alt='...'
+                position='top'
+              />
+              <MDBCardBody>
+                <MDBCardTitle>Shrimp and Olive</MDBCardTitle>
+                <MDBBtn outline color='success'>$19.00</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBCol>
+
+      </MDBRow>
     </div>
   )
 }
